ea97eafdc37dc88303e514965d03d4ef8883568b
codigo-pl <kamil.jastrzebski@gmail.com>
2013-12-21 18:47:29 +0100

    add config subgenerator

---
+++ config/index.js
@@ -0,0 +1,42 @@
+'use strict';
+var path = require('path');
+var util = require('util');
+var yeoman = require('yeoman-generator');
+
+
+var Generator = module.exports = function Generator() {
+  yeoman.generators.Base.apply(this, arguments);
+};
+
+util.inherits(Generator, yeoman.generators.Base);
+
+Generator.prototype.askForStructureType = function askForStructureType() {
+  var cb = this.async();
+
+  this.prompt([{
+    type: 'list',
+    name: 'structureType',
+    message: 'How would you like divide your project?',
+    choices: [{
+      value: 'component',
+      name: 'by component'
+    }, {
+      value: 'feature',
+      name: 'by feature'
+    }],
+    default: 0
+  }], function (props) {
+    this.copy('config-' + props.structureType + '.json',
+        path.join(process.cwd(), 'config.json'));
+
+    cb();
+  }.bind(this));
+};
+
+/*Generator.prototype.setupEnv = function setupEnv() {
+  // Copies the contents of the generator `templates`
+  // directory into your users new application path
+  this.sourceRoot(path.join(__dirname, '../templates/common'));
+  this.directory('root', '.', true);
+  this.copy('gitignore', '.gitignore');
+};*/
